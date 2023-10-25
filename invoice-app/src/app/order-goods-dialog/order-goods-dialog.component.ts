427916aec8a268363d304d7ac403a4755986e978
Diana Gatwiri <99605339+Dianedylan@users.noreply.github.com>
2023-10-25 15:36:35 +0300

    Updated paymt duedate to 30-days from invoice date

--- invoice-app/src/app/order-goods-dialog/order-goods-dialog.component.ts
+++ invoice-app/src/app/order-goods-dialog/order-goods-dialog.component.ts
@@ -55,9 +55,9 @@ export class OrderGoodsDialogComponent implements OnInit {
           });
       } else {
         const payload = {
-          ...this.orderForm.value,
           invoice_number : this.generateUniqueInvNumber(),
-          payment_due: this.createpaymentduedate()
+          ...this.orderForm.value,
+          payment_due: this.createPaymentDueDate(),
         }
         this._orderService.addOrder(payload).subscribe( res => {
         
@@ -85,32 +85,15 @@ export class OrderGoodsDialogComponent implements OnInit {
 
   return orderNumber;
 }
-createNewOrder() {
-  const newOrderNumber = this.generateUniqueInvNumber();
-    console.log(`New Order Number: ${newOrderNumber}`);
-}
 
-
-createpaymentduedate(){
-  // Create a new Date object for the current date
-const currentDate = new Date();
-
-// Add 30 days to the current date
-currentDate.setDate(currentDate.getDate() + 29);
-
-// Ensure the date is in the correct 30-day format
-if (currentDate.getDate() !== 30) {
-  // Adjust the date to the last day of the current month
-  currentDate.setMonth(currentDate.getMonth() + 2, 0);
-}
-
-// Format the date as a string in the "YYYY-MM-DD" format
-const formattedDate = currentDate.toISOString().slice(0, 10);
-
-console.log('fdate',formattedDate);
-
-}
-
-
+createPaymentDueDate(): string {
+    // Addded logic to generate the payment due date as needed by getting the current date and add 30 days
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + 30);
+    const year = currentDate.getFullYear();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
 
 }
